0f9ea5594af37bcac6377cb47966fcb6c34f3316
Roshan CN <72539879+roshanCN@users.noreply.github.com>
2021-09-24 13:49:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <SearchBar />
+      </div>
+    </div>
+  );
+}
---
+++ src/historyData.js
@@ -0,0 +1,31 @@
+const initialHistory = [
+  {
+    text: "test",
+    icon:
+      "https://rukminim1.flixcart.com/www/100/100/promos/19/07/2018/321e89f8-9ffa-47a7-a9d4-731da9bde6c4.png?q=90"
+  },
+
+  {
+    text: "qwerty",
+    icon:
+      "https://rukminim1.flixcart.com/www/100/100/promos/19/07/2018/321e89f8-9ffa-47a7-a9d4-731da9bde6c4.png?q=90"
+  },
+  {
+    text: "macbook air",
+    icon:
+      "https://rukminim1.flixcart.com/image/100/100/kp5sya80/screen-guard/tempered-glass/o/v/n/apple-macbook-air-m1-13-3-inch-lightwings-original-imag3gh5xftgbpg3.jpeg?q=90"
+  },
+  {
+    text: "macbook pro",
+    icon:
+      "https://rukminim1.flixcart.com/image/100/100/kp5sya80/screen-guard/tempered-glass/o/v/n/apple-macbook-air-m1-13-3-inch-lightwings-original-imag3gh5xftgbpg3.jpeg?q=90"
+  },
+  {
+    text: "t shirt",
+    icon:
+      "https://rukminim1.flixcart.com/image/100/100/khmbafk0-0/t-shirt/w/j/s/xs-t322-blwh-seven-rocks-original-imafxhagtyj2xvpn.jpeg?q=90"
+  }
+];
+//search-server-icon "https://rukminim1.flixcart.com/www/100/100/promos/19/07/2018/cc6daa23-e09c-40d8-a4e1-ead447bf80fa.png?q=90"
+
+export default initialHistory;
